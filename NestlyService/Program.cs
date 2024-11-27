5e4b7d907a4e8a2530fcee47207d409ca85b9da6
Szymon Gryszko <gryszkoszymon@gmail.com>
2024-11-27 10:54:18 +0100

    added standard api middleware

--- NestlyService/Program.cs
+++ NestlyService/Program.cs
@@ -1,3 +1,7 @@
+using AspNetCoreRateLimit;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,19 +11,67 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Versioning
+builder.Services.AddApiVersioning(options =>
+{
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.ReportApiVersions = true;
+});
+
+//Serilog
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File(builder.Configuration["Logging:Path"], rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+//HealthCheck
+builder.Services.AddHealthChecks();
+
+//CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin().DisallowCredentials()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+//Rate Limiting
+builder.Services.AddMemoryCache();
+builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+builder.Services.AddInMemoryRateLimiting();
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nestly API v1");
+});
+
+
+app.Use(async (context, next) =>
+{
+    Log.Information("Request: {Method} {Path}", context.Request.Method, context.Request.Path);
+    await next.Invoke();
+});
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHealthChecks("/health");
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors("AllowAll");
+
+app.UseIpRateLimiting();
 
 app.Run();
